dd8dd67e8daabff2e669badd31f323a75fc4ca87
Carlos Caceres <kalicaceres1@gmail.com>
2025-12-17 12:08:09 -0300

    Conexion entre paginas

--- FotocopiadoraWPF/MainWindow.xaml.vb
+++ FotocopiadoraWPF/MainWindow.xaml.vb
@@ -1,6 +1,26 @@
-﻿
+﻿Imports Syncfusion.UI.Xaml.NavigationDrawer
 
-Class MainWindow
+Namespace FotocopiadoraWPF
 
+    Partial Class MainWindow
+        Inherits Window
 
-End Class
+        Private Sub NavigationDrawer_ItemClicked(sender As Object, e As NavigationItemClickedEventArgs) _
+            Handles navigationDrawer.ItemClicked
+
+            Dim clickedItem As NavigationItem = TryCast(e.Item, NavigationItem)
+            If clickedItem Is Nothing Then Exit Sub
+
+            Select Case clickedItem.Header.ToString()
+                Case "Fotocopias"
+                    MainContent.Content = New FotocopiasView()
+
+                Case "Balance"
+                    MainContent.Content = New BalanceView()
+            End Select
+
+        End Sub
+
+    End Class
+
+End Namespace
